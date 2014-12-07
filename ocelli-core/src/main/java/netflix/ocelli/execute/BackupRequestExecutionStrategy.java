c55a04852f129dcc85920cd9fdc08ad46bef1de4
elandau <elandau@yahoo.com>
2014-12-06 17:35:55 -0800

    Optimize BackupRequestExecutionStrategy to minimize overhead from RxJava

--- ocelli-core/src/main/java/netflix/ocelli/execute/BackupRequestExecutionStrategy.java
+++ ocelli-core/src/main/java/netflix/ocelli/execute/BackupRequestExecutionStrategy.java
@@ -1,12 +1,23 @@
 package netflix.ocelli.execute;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import netflix.ocelli.LoadBalancer;
 import netflix.ocelli.loadbalancer.DefaultLoadBalancer;
 import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Observer;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.functions.Action0;
 import rx.functions.Func0;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
+import rx.subscriptions.Subscriptions;
 
 /**
  * Execution strategy that executes on one host but then tries one more host
@@ -21,25 +32,104 @@ import rx.functions.Func1;
  * @param <C>
  */
 public class BackupRequestExecutionStrategy<C> extends ExecutionStrategy<C> {
+    public static Func1<Throwable, Boolean> ALWAYS = new Func1<Throwable, Boolean>() {
+        @Override
+        public Boolean call(Throwable e) {
+            return true;
+        }
+    };
     
     private LoadBalancer<C> lb;
     private Func0<Integer> delay;
-
-    public BackupRequestExecutionStrategy(final LoadBalancer<C> lb, final Func0<Integer> delay) {
+    private Func1<Throwable, Boolean> retriableError;
+    private Scheduler scheduler = Schedulers.computation();
+    private TimeUnit units = TimeUnit.MILLISECONDS;
+    
+    public BackupRequestExecutionStrategy(final LoadBalancer<C> lb, final Func0<Integer> delay, Func1<Throwable, Boolean> retriableError) {
         this.lb = lb;
         this.delay = delay;
+        this.retriableError = retriableError;
+    }
+    
+    public BackupRequestExecutionStrategy(final LoadBalancer<C> lb, final Func0<Integer> delay) {
+        this(lb, delay, ALWAYS);
     }
     
     @Override
     public <R> Observable<R> execute(final Func1<C, Observable<R>> operation) {
-        return Observable.amb(
-                lb.choose().flatMap(operation),
-                Observable.timer(delay.call(), TimeUnit.MILLISECONDS).flatMap(new Func1<Long, Observable<R>>() {
+        return Observable.create(new OnSubscribe<R>() {
+            @Override
+            public void call(final Subscriber<? super R> s) {
+                final CompositeSubscription cs = new CompositeSubscription();
+                final AtomicInteger requestCount = new AtomicInteger(0);
+                
+                // Observer for both the initial and backup request
+                final Observer<R> opObserver = new Observer<R>() {
+                    private AtomicInteger errorCount = new AtomicInteger(0);
+                    private AtomicBoolean onNextCalled = new AtomicBoolean(false);
+                    
                     @Override
-                    public Observable<R> call(Long t1) {
-                        return lb.choose().flatMap(operation);
+                    public void onCompleted() {
+                        // ok to ignore
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        if (requestCount.get() == 1 && e instanceof NoSuchElementException) {
+                            s.onError(e);
+                        }
+                        
+                        if (!retriableError.call(e) || errorCount.incrementAndGet() > 1) {
+                            s.onError(e);
+                        }
+                    }
+
+                    @Override
+                    public void onNext(R t) {
+                        if (onNextCalled.compareAndSet(false, true)) {
+                            s.onNext(t);
+                            s.onCompleted();
+                        }
+                    }
+                };
+                
+                // Choose a host and forward errors and response to opObserver
+                final Observer<C> lbObserver = new Observer<C>() {
+                    @Override
+                    public void onCompleted() {
+                        // OK to ignore.  Should never return null.
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        opObserver.onError(e);
+                    }
+
+                    @Override
+                    public void onNext(C t) {
+                        cs.add(operation.call(t).subscribe(opObserver));
+                    }
+                };
+                
+                // This is the initial request
+                cs.add(lb.choose().subscribe(lbObserver));
+                
+                // This is the backup request
+                cs.add(scheduler.createWorker().schedule(new Action0() {
+                    @Override
+                    public void call() {
+                        cs.add(lb.choose().subscribe(lbObserver));
+                    }
+                }, delay.call(), units));
+                
+                s.add(Subscriptions.create(new Action0() {
+                    @Override
+                    public void call() {
+                        cs.unsubscribe();
                     }
                 }));
+            }
+        });
     }
 
     public static <C> BackupRequestExecutionStrategy<C> create(DefaultLoadBalancer<C> lb, Func0<Integer> delay) {
