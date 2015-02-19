0794345f2459d38fe558f319180caf083ce641db
elandau <elandau@yahoo.com>
2015-02-19 13:29:10 -0800

    Clean up executors.  Remove Logging, Intercepting and FailuringInjecting interceptors in favor of moving those to Ribbon3.

--- ocelli-core/src/main/java/netflix/ocelli/executor/Executors.java
+++ ocelli-core/src/main/java/netflix/ocelli/executor/Executors.java
@@ -1,5 +1,7 @@
 package netflix.ocelli.executor;
 
+import java.util.NoSuchElementException;
+
 import rx.Observable;
 
 public class Executors {
@@ -21,11 +23,38 @@ public class Executors {
         };
     }
 
+    public static <I, O> Executor<I, O> memoize(final Observable<O> response) {
+        return new Executor<I, O>() {
+            @Override
+            public Observable<O> call(I request) {
+                return response;
+            }
+        };
+    }
+
     public static <I, O> Executor<I, O> error(final Exception exception) {
         return new Executor<I, O>() {
             @Override
             public Observable<O> call(I request) {
                 return Observable.error(exception);
+            }
+        };
+    }
+    
+    public static <I, O> Executor<I, O> empty() {
+        return new Executor<I, O>() {
+            @Override
+            public Observable<O> call(I request) {
+                return Observable.error(new NoSuchElementException());
+            }
+        };
+    }
+    
+    public static <I, O> Executor<I, O> never() {
+        return new Executor<I, O>() {
+            @Override
+            public Observable<O> call(I request) {
+                return Observable.never();
             }
         };
     }
