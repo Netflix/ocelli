565615cd77e4fda83915cb5311d8c5e0837bd56c
elandau <elandau@yahoo.com>
2015-02-23 22:26:14 -0800

    Reimplement FallbackExecutor as FallbackLoadBalancer

---
+++ ocelli-core/src/test/java/netflix/ocelli/loadbalancer/FallbackLoadBalancerTest.java
@@ -0,0 +1,57 @@
+package netflix.ocelli.loadbalancer;
+
+import java.util.NoSuchElementException;
+
+import junit.framework.Assert;
+import netflix.ocelli.LoadBalancer;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+public class FallbackLoadBalancerTest {
+    private static LoadBalancer<Integer> lb1 = RoundRobinLoadBalancer.create();
+    private static LoadBalancer<Integer> lb2 = RoundRobinLoadBalancer.create();
+    private static LoadBalancer<Integer> lb3 = RoundRobinLoadBalancer.create();
+    
+    @BeforeClass
+    public static void before() {
+        lb3.call(Lists.newArrayList(1));
+    }
+    
+    @Test
+    public void firstHasClient() {
+        FallbackLoadBalancer<Integer> lb = new FallbackLoadBalancer<Integer>(Lists.newArrayList(lb3, lb1, lb2));
+        int value = lb.toBlocking().single();
+        Assert.assertEquals(1, value);
+    }
+    
+    @Test
+    public void middleHasClient() {
+        FallbackLoadBalancer<Integer> lb = new FallbackLoadBalancer<Integer>(Lists.newArrayList(lb1, lb3, lb2));
+        int value = lb.toBlocking().single();
+        Assert.assertEquals(1, value);
+    }
+    
+    @Test
+    public void lastHasClient() {
+        FallbackLoadBalancer<Integer> lb = new FallbackLoadBalancer<Integer>(Lists.newArrayList(lb1, lb2, lb3));
+        int value = lb.toBlocking().single();
+        Assert.assertEquals(1, value);
+    }
+    
+    @Test(expected=NoSuchElementException.class)
+    public void noneHaveClient() {
+        FallbackLoadBalancer<Integer> lb = new FallbackLoadBalancer<Integer>(Lists.newArrayList(lb1, lb2, lb2));
+        int value = lb.toBlocking().single();
+        Assert.assertEquals(1, value);
+    }
+    
+    @Test(expected=NoSuchElementException.class)
+    public void noLbs() {
+        FallbackLoadBalancer<Integer> lb = new FallbackLoadBalancer(Lists.newArrayList());
+        int value = lb.toBlocking().single();
+        Assert.assertEquals(1, value);
+    }
+}
